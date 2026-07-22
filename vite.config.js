2f5bf978cf890ac9c29095a2e160badb6c2ace95
hshmaker <man12297565@gmail.com>
2026-07-22 22:59:36 +0900

    page split22

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/README-Bricks/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "main.html"),
+        index: resolve(__dirname, "index.html"),
+      },
+    },
+  },
 });
